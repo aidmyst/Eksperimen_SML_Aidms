bf51b9a0931c51606be64be61ec02bbd9ba4c5e0
Syahid-IT <dihays125@gmail.com>
2026-07-26 04:15:27 +0700

    Change dataset from Wine Quality to Diabetes

--- preprocessing/automate_Aidms.py
+++ preprocessing/automate_Aidms.py
@@ -3,9 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 def load_and_preprocess_data(file_path):
-    df = pd.read_csv(file_path, sep=';')
-    X = df.drop('quality', axis=1)
-    y = df['quality']
+    df = pd.read_csv(file_path)
+    X = df.drop('target', axis=1)
+    y = df['target']
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     scaler = StandardScaler()
@@ -15,5 +15,5 @@ def load_and_preprocess_data(file_path):
     return X_train_scaled, X_test_scaled, y_train, y_test
 
 if __name__ == '__main__':
-    X_train, X_test, y_train, y_test = load_and_preprocess_data('../namadataset_raw/winequality-red.csv')
+    X_train, X_test, y_train, y_test = load_and_preprocess_data('../namadataset_raw/diabetes.csv')
     print("Preprocessing complete.")
